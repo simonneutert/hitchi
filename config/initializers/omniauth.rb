d18bb8828d4c20f7b9624991c160d07cf3d1b698
Simon Neutert <Simon.Neutert@gmail.com>
2019-04-10 21:48:44 +0200

    drops omniauth

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_API_KEY'], ENV['FB_API_SECRET'], {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
-end

72f1d678933eda2bd81173e65068c0e300e856b1
Simon Neutert <Simon.Neutert@gmail.com>
2019-04-11 06:36:32 +0200

    server

--- db/migrate/20170425105715_add_index_to_fb_parses_where_status_true.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToFbParsesWhereStatusTrue < ActiveRecord::Migration[5.0]
-  def change
-    add_index :fb_parses, :status, name: "index_statuses_on_true", where: "(status IS TRUE)"
-  end
-end
